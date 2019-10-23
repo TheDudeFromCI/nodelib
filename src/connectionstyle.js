ad1d04aedcd14e8dc0bc40789ff8e2dc5117da76
TheDudeFromCI <thedudefromci@gmail.com>
2019-10-23 12:11:32 -0700

    Added ZLinear connection rendering style.

--- src/connectionstyle.js
+++ src/connectionstyle.js
@@ -18,3 +18,21 @@ NodeGraph.ConnectionStyle.Linear = function(outputPlug, inputPlug, ctx,
 	ctx.lineTo(b.x, b.y);
 	ctx.stroke();
 }
+
+/*
+ * Renders a straight line between two plugs, where the the line angles shortly
+ * before and after the plug into a horizontal line into a semi z-shape.
+ */
+NodeGraph.ConnectionStyle.ZLinear = function(outputPlug, inputPlug, ctx,
+	camera)
+{
+	let a = outputPlug.pos.toScreen(camera);
+	let b = inputPlug.pos.toScreen(camera);
+
+	ctx.beginPath();
+	ctx.moveTo(a.x, a.y);
+	ctx.lineTo(a.x + 25 * camera.zoomSmooth, a.y);
+	ctx.lineTo(b.x - 25 * camera.zoomSmooth, b.y);
+	ctx.lineTo(b.x, b.y);
+	ctx.stroke();
+}
